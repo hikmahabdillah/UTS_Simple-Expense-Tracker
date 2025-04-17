6400e0eb94c9e9ad3ecfaa3022453c3ca7e2c5ec
hikmahabdillah <hikmahaldrin44@gmail.com>
2025-04-17 20:35:45 +0700

    setup model and controller, install tailwind animated, and create migrations

---
+++ database/migrations/2025_04_17_124546_create_expenses_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('expenses', function (Blueprint $table) {
+            $table->id(); // BIGINT auto increment, primary key
+            $table->string('title'); // VARCHAR
+            $table->decimal('amount', 12, 2); // Decimal(12,2)
+            $table->enum('category', [ // ENUM
+                'food',
+                'transportation',
+                'bills',
+                'entertainment',
+                'health',
+                'education',
+                'others',
+            ]);
+            $table->enum('type', ['income', 'expense']); // ENUM
+            $table->text('description')->nullable(); // TEXT nullable
+            $table->timestamps(); // created_at & updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('expenses');
+    }
+};
